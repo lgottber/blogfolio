94e2dccfbac2b9a935f43577ea9fbb1fc1f46e0d
lgottber <lgottber@gmail.com>
2026-04-20 18:06:06 -0400

    clean up

--- src/lib/weekly.ts
+++ src/lib/weekly.ts
@@ -8,11 +8,7 @@ export interface Song {
   spotifyId?: string;
 }
 
-export interface WeeklyData {
-  songs: { current: Song; herstory: Song[] };
-}
-
-export function getWeeklyData(): WeeklyData {
+export function getWeeklyData(): { songs: { current: Song; herstory: Song[] } } {
   return {
     songs: songsData as { current: Song; herstory: Song[] },
   };
